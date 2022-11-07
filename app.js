887af3d47db944e3d821ac47f22f5c800236c865
saanghyuk <saanghyuk@gmail.com>
2022-11-07 20:44:33 +0900

    GET /

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+var path = require("path");
 
+// const path = require("path");
+
+// __dirname = "front";
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 const db = require("./models/index");
 const { Member } = db;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.get("/api/members", async (req, res) => {
   const { team } = req.query;
