aa7a491ea10b736127e0762d2d4e916ea54a871c
saanghyuk <saanghyuk@gmail.com>
2022-11-08 01:33:55 +0900

    fix html link

--- seeders/20221107065303-initialMembers.js
+++ seeders/20221107065303-initialMembers.js
@@ -8,7 +8,7 @@ module.exports = {
       [
         {
           id: 1,
-          name: "정하은",
+          name: "Heidi Jung",
           team: "Account Development",
           position: "Account Development Manager",
           emailAddress: "heidi@ab180.co",
@@ -16,7 +16,7 @@ module.exports = {
         },
         {
           id: 2,
-          name: "강미선",
+          name: "Sunny Kang",
           team: "Account Development",
           position: "Account Development Manager",
           emailAddress: "sunny.kang@ab180.co",
@@ -24,7 +24,7 @@ module.exports = {
         },
         {
           id: 3,
-          name: "김여은",
+          name: "Yeoeun Kim",
           team: "Enablement",
           position: "Enablement Manager",
           emailAddress: "yeoeun@ab180.co",
@@ -32,7 +32,7 @@ module.exports = {
         },
         {
           id: 4,
-          name: "남성필",
+          name: "Roi Nam",
           team: "All",
           position: "Chief Executive Officer",
           emailAddress: "nam@ab180.co",
@@ -40,7 +40,7 @@ module.exports = {
         },
         {
           id: 5,
-          name: "강다현",
+          name: "Dana Kang",
           team: "Marketing",
           position: "Product Marketing Manager",
           emailAddress: "dahyun@ab180.co",
@@ -48,7 +48,7 @@ module.exports = {
         },
         {
           id: 6,
-          name: "이재문",
+          name: "Jaemoon Lee",
           team: "Marketing",
           position: "Marketing Team Lead",
           emailAddress: "moon@ab180.co",
@@ -56,7 +56,7 @@ module.exports = {
         },
         {
           id: 7,
-          name: "이민아",
+          name: "Minah Lee",
           team: "Marketing",
           position: "Product Marketing Manager",
           emailAddress: "minah@ab180.co",
@@ -64,7 +64,7 @@ module.exports = {
         },
         {
           id: 8,
-          name: "권기현",
+          name: "Keehyun Kwon",
           team: "Account Executive",
           position: "Head of Sales",
           emailAddress: "keehyun@ab180.co",
@@ -72,7 +72,7 @@ module.exports = {
         },
         {
           id: 9,
-          name: "김태윤",
+          name: "Tai Kim",
           team: "SOLCON",
           position: "SOLCON Team Lead",
           emailAddress: "tai@ab180.co",
@@ -80,7 +80,7 @@ module.exports = {
         },
         {
           id: 10,
-          name: "김효은",
+          name: "Hyoeun Kim",
           team: "Sales",
           position: "Sales Operation Manager",
           emailAddress: "hyoeun@ab180.co",
@@ -88,7 +88,7 @@ module.exports = {
         },
         {
           id: 11,
-          name: "전온유",
+          name: "Onyoo Jeon",
           team: "Brand Design",
           position: "Brand Designer",
           emailAddress: "onyoo@ab180.co",
@@ -96,11 +96,35 @@ module.exports = {
         },
         {
           id: 12,
-          name: "배준휘",
+          name: "June Bae",
           team: "Partnership Development",
           position: "Partnership Development Manager",
           emailAddress: "june@ab180.co",
           profileImage: "profile12.png",
+        },
+        {
+          id: 13,
+          name: "Mu Song",
+          team: "Product Design",
+          position: "Product Writer",
+          emailAddress: "jusang@ab180.co",
+          profileImage: "profile13.png",
+        },
+        {
+          id: 14,
+          name: "Sojeen Park",
+          team: "Customer Success 2",
+          position: "Customer Success 2 Team Lead",
+          emailAddress: "sojeen@ab180.co",
+          profileImage: "profile14.png",
+        },
+        {
+          id: 15,
+          name: "Hojin Yoo",
+          team: "Globalization",
+          position: "Translator",
+          emailAddress: "hojin@ab180.co",
+          profileImage: "profile15.png",
         },
       ],
       {}
